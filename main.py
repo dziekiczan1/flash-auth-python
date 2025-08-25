4c725daf37c2da29ae0b92a97a9f7b1979b6d25a
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-08-25 19:56:38 +0200

    register user

--- main.py
+++ main.py
@@ -38,8 +38,20 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        new_user = User(
+            email=request.form.get('email'),
+            name=request.form.get('name'),
+            password=request.form.get('password')
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return render_template("secrets.html", name=request.form.get('name'))
+
     return render_template("register.html")
 
 
@@ -60,7 +72,7 @@ def logout():
 
 @app.route('/download')
 def download():
-    pass
+    return send_from_directory('static', path="files/cheat_sheet.pdf")
 
 
 if __name__ == "__main__":
